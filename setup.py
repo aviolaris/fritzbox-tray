ea0499215cd5ba36a1e9cea0ebb231e703afbb31
Andreas Violaris <andreas.v.violaris@gmail.com>
2023-01-09 22:18:20 +0200

    Fixed C0114 & W1514.

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
+"""
+FritzBox Tray - A system tray application for interacting with FRITZ!Box devices.
+Copyright (C) 2023 Andreas Violaris
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
 import os
 from setuptools import setup, find_namespace_packages
 
@@ -5,7 +22,7 @@ with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
                        'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-with open('requirements.txt') as f:
+with open('requirements.txt', encoding='utf-8') as f:
     requirements = f.read().splitlines()
 
 setup(
