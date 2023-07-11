7265f220e94490a83c8c0db4d52ccacbe4625186
Andreas Violaris <48277853+aviolaris@users.noreply.github.com>
2023-07-11 20:42:36 +0300

    Bump charset-normalizer from 3.1.0 to 3.2.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('requirements.txt', encoding='utf-8') as f:
 
 setup(
     name='fritzbox-tray',
-    version='1.0.60',
+    version='1.0.61',
     packages=find_namespace_packages(where="src"),
     package_dir={"": "src"},
     package_data={
