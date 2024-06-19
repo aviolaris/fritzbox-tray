8cef1f9984118589c50ec178ccf2ffdcb8c49a0b
Andreas Violaris <48277853+aviolaris@users.noreply.github.com>
2024-06-20 02:32:18 +0300

    Bump fritzbox-tray from 1.0.69 to 1.0.70

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('requirements.txt', encoding='utf-8') as f:
 
 setup(
     name='fritzbox-tray',
-    version='1.0.69',
+    version='1.0.70',
     packages=find_namespace_packages(where="src"),
     package_dir={"": "src"},
     package_data={
